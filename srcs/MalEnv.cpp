f0afbcc87a0354e43c7993da60ac3631e28f8e58
0x35c <unai64@orange.fr>
2024-05-05 14:16:29 +0200

    i find it cleaner with map[a] = b

--- srcs/MalEnv.cpp
+++ srcs/MalEnv.cpp
@@ -2,30 +2,26 @@
 
 MalEnv::MalEnv()
 {
-	m_map.insert(
-	    std::make_pair("+", new MalFunc([](MalType *a, MalType *b) {
-				   return dynamic_cast<MalType *>(new MalNumber(
-				       std::atoi(a->str(true).c_str()) +
-				       std::atoi(b->str(true).c_str())));
-			   })));
-	m_map.insert(
-	    std::make_pair("-", new MalFunc([](MalType *a, MalType *b) {
-				   return dynamic_cast<MalType *>(new MalNumber(
-				       std::atoi(a->str(true).c_str()) -
-				       std::atoi(b->str(true).c_str())));
-			   })));
-	m_map.insert(
-	    std::make_pair("*", new MalFunc([](MalType *a, MalType *b) {
-				   return dynamic_cast<MalType *>(new MalNumber(
-				       std::atoi(a->str(true).c_str()) *
-				       std::atoi(b->str(true).c_str())));
-			   })));
-	m_map.insert(
-	    std::make_pair("/", new MalFunc([](MalType *a, MalType *b) {
-				   return dynamic_cast<MalType *>(new MalNumber(
-				       std::atoi(a->str(true).c_str()) /
-				       std::atoi(b->str(true).c_str())));
-			   })));
+	m_map["+"] = new MalFunc([](MalType *a, MalType *b) {
+		return dynamic_cast<MalType *>(
+		    new MalNumber(std::atoi(a->str(true).c_str()) +
+		                  std::atoi(b->str(true).c_str())));
+	});
+	m_map["-"] = new MalFunc([](MalType *a, MalType *b) {
+		return dynamic_cast<MalType *>(
+		    new MalNumber(std::atoi(a->str(true).c_str()) -
+		                  std::atoi(b->str(true).c_str())));
+	});
+	m_map["*"] = new MalFunc([](MalType *a, MalType *b) {
+		return dynamic_cast<MalType *>(
+		    new MalNumber(std::atoi(a->str(true).c_str()) *
+		                  std::atoi(b->str(true).c_str())));
+	});
+	m_map["/"] = new MalFunc([](MalType *a, MalType *b) {
+		return dynamic_cast<MalType *>(
+		    new MalNumber(std::atoi(a->str(true).c_str()) /
+		                  std::atoi(b->str(true).c_str())));
+	});
 };
 
 MalType *MalEnv::find(const String &symbol)
